16efba5f606d45eb7e6fbb47d6a4d1a194902922
DukuPc4 <dukudenis@gmail.com>
2020-07-30 20:41:06 -0500

    first commit

---
+++ auto_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get -y install apache2 php7.0 libapache2-mod-php7.0
+service apache2 start
+
+cat <<EOF > /var/www/html/index.php
+<?php
+\$hostname = gethostname();
+echo "<h2>Hello World!</h2>";
+echo "<h2>Server Hostname: \$hostname</h2>";
+?>
+EOF
+
+rm /var/www/html/index.html
+
+service apache2 restart
